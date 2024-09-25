02d5ccfbff39c3e27b662f4463b8be1af44b61b9
JavierShepard <comander.javier@gmail.com>
2024-09-24 21:10:31 -0300

    update

--- config/db.js
+++ config/db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'rotiseria'
+  host: process.env.DB_HOST,       // Aquí debe ir el host de tu base de datos externa
+  user: process.env.DB_USER,       // Usuario de la base de datos
+  password: process.env.DB_PASSWORD, // Contraseña
+  database: process.env.DB_NAME,   // Nombre de la base de datos
+  port: process.env.DB_PORT || 3306 /
 });
 
 connection.connect((err) => {
