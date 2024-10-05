c5d500e54e2396159b242e5b8f9e111813e551a7
JavierShepard <comander.javier@gmail.com>
2024-10-05 16:57:59 -0300

    update 05-10-2024

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-// app.js
 const express = require('express');
 const helmet = require('helmet');
-const app = express();
 const cors = require('cors');
-
+const app = express();
 const productoRoutes = require('./routes/productoRoutes');
 const comandaRoutes = require('./routes/comandaRoutes');
 require('dotenv').config();
-// Configuración del middleware CORS
-app.use(cors({
-  origin: 'http://localhost:5174',  // Permite solicitudes desde el frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+
+// Lista de orígenes permitidos
+const allowedOrigins = ['http://localhost:5174', 'http://localhost:5173', 'https://rotiserialatriada.onrender.com'];
+
+// Configuración del middleware CORS dinámico
+app.use(cors((req, callback) => {
+  let corsOptions;
+  let origin = req.header('Origin'); // Obtiene el origen de la solicitud
+
+  // Verifica si el origen de la solicitud está en la lista de orígenes permitidos
+  if (allowedOrigins.includes(origin)) {
+    corsOptions = { origin: true }; // Permite este origen
+  } else {
+    corsOptions = { origin: false }; // Bloquea otros orígenes
+  }
+  callback(null, corsOptions); // Devuelve la configuración de CORS
 }));
+
 // Configurar Helmet con una política CSP personalizada
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      fontSrc: ["'self'", "https://rotiserialatriada.onrender.com"], // Permitir fuentes desde tu dominio
-      styleSrc: ["'self'", "'unsafe-inline'"], // Permitir estilos en línea si es necesario
+      fontSrc: ["'self'", "https://rotiserialatriada.onrender.com"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
     }
   }
 }));
 
-// Middleware
+// Middleware para parsear JSON
 app.use(express.json());
 
 // Rutas
 app.use('/api', productoRoutes);
 app.use('/api', comandaRoutes);
 
+// Inicialización del servidor
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
