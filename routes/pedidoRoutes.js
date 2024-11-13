d13a09234ed3706a4918325d8f2d1abaae92f937
JavierShepard <comander.javier@gmail.com>
2024-11-12 23:10:32 -0300

    update 29-10-2024

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.get('/pedidos', pedidoController.getAllPedidos);
 router.get('/pedidos/:id', pedidoController.getPedidoById);
 router.post('/pedidos', authMiddleware, pedidoController.createPedido);
-router.patch('/pedidos/:id', authMiddleware, pedidoController.updatePedido);
+router.put('/pedidos/:id', authMiddleware, pedidoController.updatePedido);
 router.delete('/pedidos/:id', authMiddleware, pedidoController.deletePedido);
 
 module.exports = router;
